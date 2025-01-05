7236fee0750babf76a29fbe4c273dbd6dd751175
Sudeep Padalkar <sudeep.padalkar@tamara.co>
2025-01-05 01:56:21 +0100

    added bigbarks

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sudeep-tamara/puppy
 
 go 1.22.3
+
+require github.com/sudeep-tamara/dog v0.0.0-20250105005300-68ac57a9c8cc // indirect
--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package puppy
 
+import "github.com/sudeep-tamara/dog"
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
 }
